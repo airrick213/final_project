3b48f6770fd5723f1c86a01d9c9622d3a49718a6
airrick213 <erickim213@gmail.com>
2014-07-18 12:01:55 -0400

    website working

--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require 'pry'
 require './lib/final_project.rb'
 
 get '/' do
-	scraper = CalendarWeather.new
-	@calendar = scraper.view_calendar
-	scraper.add_item(@date, @action, @place)
-	scraper.delete_item(@delete_target)
-	scraper.weather_report(@email)
+	@scraper = CalendarWeather.new
+	@calendar = @scraper.view_calendar
+	@date = nil
+	@action = nil
+	@place = nil
+	@delete_target = nil
+	@email = nil
 	erb :index
 end
 
@@ -18,5 +20,14 @@ post '/' do
 	@place = params[:place]
 	@delete_target = params[:delete_target]
 	@email = params[:email]
+	@scraper = CalendarWeather.new
+	if @date != nil
+		@scraper.add_item(@date, @action, @place)
+	elsif @delete_target != nil
+		@scraper.delete_item(@delete_target)
+	elsif @email != nil
+		@scraper.weather_report(@email)
+	end
+	@calendar = @scraper.view_calendar
 	erb :index, :locals => {'date' => @date, 'action' => @action, 'place' => @place, 'delete_target' => @delete_target, 'email' => @email}
 end
